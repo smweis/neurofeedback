6ce069e4b493ee663305aed3a34b535a2533a4a9
iron <iron@irons-mbp.uphs.upenn.edu>
2018-09-25 14:23:06 -0400

    updating for mac

--- register_EPI_to_EPI.sh
+++ register_EPI_to_EPI.sh
@@ -1,13 +1,13 @@
-# This script will take a participant's AP/PA bold scans and register a parcel to a sample scan. 
-# Step 1. Transfer V1_TO_PA.nii.gz, V1_TO_AP.sh, AP_first_volume.nii.gz, PA_first_volume.nii.gz to the neurofeedback comp. 
+# This script will take a participant's AP/PA bold scans and register a parcel to a sample scan.
+# Step 1. Transfer V1_TO_PA.nii.gz, V1_TO_AP.sh, AP_first_volume.nii.gz, PA_first_volume.nii.gz to the neurofeedback comp.
 # Step 2. Type in directories as indicated below. Rename the new AP/PA nifti file "newPA.nii" or "newAP.nii"
 
 
-# to run this from command line: 
+# to run this from command line:
 # bash register_EPI_to_EPI.sh AP
 
 #FILL IN DIRECTORY HERE
-olddir='PUT IN old file dir NAME HERE'/data/jux/sweisberg/neurofeedback/subjects/;
+olddir='/Users/iron/Documents/neurofeedback/TOME_3040';
 
 
 #FILL IN DIRECTORY HERE
@@ -25,14 +25,8 @@ bet $newdir/$newNifti $newdir/$newNifti
 # registration
 ##########################
 
-# register first volume of functional scan to MPRAGE
+# register first volume of old functional scan to new functional scan
 flirt -in $olddir/"$1"_first_volume.nii -ref $newdir/$newNifti -omat new2old"$1".mat -bins 256 -cost corratio -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -dof 6
 
 # apply registration to kastner parcel(s)
-flirt -in $olddir/V1_TO_"$1".nii -ref "$1"_first_volume.nii -out $newdir/V1_TO_new"$1".nii -applyxfm -init new2old"$1".mat -interp trilinear 
-
-
-
-
-
-
+flirt -in $olddir/V1_TO_"$1".nii -ref "$1"_first_volume.nii -out $newdir/V1_TO_new"$1".nii -applyxfm -init new2old"$1".mat -interp trilinear
--- wrapper.m
+++ wrapper.m
@@ -1,7 +1,7 @@
 % Change path to where NIFTIs will be stored on Macbook
 global niftiPath
 
-niftiPath = 'E:\neurofeedback_test\data\testdata\niftis\';
+niftiPath = '/Users/iron/Documents/neurofeedback/TOME_3040/niftis';
 
 global iteration
 iteration = 1;
@@ -12,5 +12,3 @@ figure;
 
 
 check_for_new_dicom
-
-
