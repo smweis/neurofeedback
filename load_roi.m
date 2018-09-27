d0e4aa4e9688b7225c1f7991df11408a0866911b
iron <iron@huntsman-ve702-7353.apng.wlan.private.upenn.edu>
2018-09-27 13:25:21 -0400

    making mac version!

--- load_roi.m
+++ load_roi.m
@@ -1,15 +1,17 @@
+global v1Index
+
+sequence_order = input('Is this a PA or AP scan?','s');
 
 % This is where you will load the Nifti file that is V1_TO_new[PA,AP].nii
-% roiPath = PATH_TO_NIFTI
-% roiNifti = load_untouch_nii(roiPath);
-% v1Index = roiNifti.img;
-% v1Index = logical(v1Index);
-
-
-
-global v1Index 
-v1Index = zeros(96,96,80);
-v1Index(45:55,45:55,45:55) = 1;
+roiPath = strcat('/Users/iron/Documents/neurofeedback/TOME_3040/V1_TO_new',sequence_order,'.nii.gz');
+roiNifti = load_untouch_nii(roiPath);
+v1Index = roiNifti.img;
 v1Index = logical(v1Index);
+
+
+% sham region
+%v1Index = zeros(96,96,80);
+%v1Index(45:55,45:55,45:55) = 1;
+%v1Index = logical(v1Index);
 
 
--- plot_at_scanner.m
+++ plot_at_scanner.m
@@ -6,16 +6,22 @@ global niftiPath
 
 acqTime = datetime; %save timepoint  
 
+newNiftiPath = strcat(niftiPath,'/',niftiName);
 
 % Step 1. Convert DICOM to NIFTI (.nii) and load NIFTI into Matlab
-dicm2nii(dicomPath,strcat(niftiPath,niftiName),0);
-targetNifti = load_untouch_nii(strcat(niftiPath,niftiName,'/BOLD_RUN1.nii'));
+dicm2nii(strcat(dicomPath,'/',niftiName),newNiftiPath,0);
+newNiftiName = dir(strcat(newNiftiPath,'/*.nii'));
+newNiftiName = strcat(newNiftiPath,'/',newNiftiName.name);
+targetNifti = load_untouch_nii(newNiftiName);
+
 targetIm = targetNifti.img;
 
 % Step 2. Compute mean from v1 ROI, then plot it against a timestamp
 v1Signal = mean(targetIm(v1Index));
     
 dataTimepoint = datetime;
+
+
 
 end
 
