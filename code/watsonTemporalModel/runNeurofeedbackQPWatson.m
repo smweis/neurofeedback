8b4180d9068e8d5c2c20e255cba3b380ceec5395
nfuser <nfuser@huntsman-ve501-0341.apn.wlan.upenn.edu>
2018-12-19 15:07:09 -0500

    adding a sample stim generator and finishing neurofeedback for quest+ and tfe

---
+++ code/watsonTemporalModel/runNeurofeedbackQPWatson.m
@@ -0,0 +1,147 @@
+
+
+
+%% Either edit these variables here, or input them each time.
+%subject = input('Subject number?','s');
+%run = input('Which run?','s');
+subject = 'TOME_3040_TEST';
+run = '1';
+
+
+
+% set flags
+showFig = false;
+checkForTrigger = true;
+registerToFirst = true;
+
+% initialize figure
+if showFig
+    figure;
+end
+
+%% Get Relevant Paths
+
+[subjectPath, scannerPath, codePath, scratchPath] = getPaths(subject);
+
+
+%% Check for trigger
+
+if checkForTrigger
+    first_trigger_time = waitForTrigger;
+end
+
+%% Register to First DICOM
+
+if registerToFirst
+    [ap_or_pa,initialDirSize] = registerToFirstDicom(subject,subjectPath,run,scannerPath,codePath);
+end
+
+%% Load the ROI HERE!
+
+roiName = ['ROI_to_new',ap_or_pa,'_bin.nii.gz'];
+roiPath = fullfile(subjectPath,roiName);
+roiIndex = loadRoi(roiPath);
+
+%% Initialize Quest and TFE
+
+initializeQuestAndTFE;
+
+
+%% Main Neurofeedback Loop
+
+% Initialize the main data struct;
+mainData = struct;
+mainData.acqTime = {}; % time at which the DICOM hit the local computer
+mainData.dataTimepoint = {}; % time at which the DICOM was processed
+mainData.dicomName = {}; % name of the DICOM
+mainData.roiSignal = {}; % whatever signal is the output (default is mean)
+
+
+% This script will check for a new DICOM, then call scripts that will
+% convert it to NIFTI, and do some processing on the NIFTI.
+% (Extract ROI, compute mean signal of the ROI).
+
+roiSignal = [];
+
+i = 0;
+j = 1;
+while i < 10000000000
+    i = i + 1;
+    
+    % Check for a new DICOM. This will run until there is a new one. 
+    [mainData(j).acqTime,mainData(j).dataTimepoint,mainData(j).roiSignal,...
+     initialDirSize, mainData(j).dicomName] = ...
+     checkForNewDicom(scannerPath,roiIndex,initialDirSize,scratchPath);
+ 
+ 
+ 
+    % Vectorize roiSignal
+    for j = 1:length(mainData)
+        for k = 1:length(mainData(j).roiSignal)
+            roiSignal(end+1) = mainData(j).roiSignal(k);
+        end
+    end
+ 
+    % Mean center roiSignal and take out linear trends
+    roiSignal = detrend(roiSignal);
+    roiSignal = detrend(roiSignal,'constant');
+ 
+    
+    % update thePacket for TFE
+    thePacket.response.values(1:length(roiSignal)) = roiSignal;
+    
+    % run TFE
+    params = temporalFit.fitResponse(thePacket,...
+        'defaultParamsInfo', defaultParamsInfo, ...
+        'searchMethod','linearRegression');
+    
+    % get the latest list of actual Stimuli that have been run
+    s = dir(fullfile(subjectPath,'actualStimuli.txt'));
+    
+    if s.bytes == 0
+        continue;
+    else
+        fidr = fopen(fullfile(subjectPath,'actualStimuli.txt'),'r');
+        actualStims = fscanf(fidr,'%d');
+        fclose(fidr);
+    end
+    
+    
+    % re-start the questData structure from when it was initialized;
+    questData = questDataCopy;
+    
+    
+    
+    % adjust pctBOLDbins if necessary creating 20 bins, evenly spaced, 
+    % between the observed min and max
+    if min(params.paramMainMatrix) < min(pctBOLDbins)
+        pctBOLDbinsMin = min(params.paramMainMatrix) - .01;
+    end
+    
+    if max(params.paramMainMatrix) > max(pctBOLDbins)
+        pctBOLDbinsMax = max(params.paramMainMatrix) + .01;
+    end
+    pctBOLDbins = pctBOLDbinsMin:(pctBOLDbinsMax-pctBOLDbinsMin)/20:pctBOLDbinsMax;
+    
+    
+    % run Q+ update with every stim so far and every outcome as given by TFE
+    for k = 1:length(actualStims)
+        outcome = discretize(params.paramMainMatrix(k),pctBOLDbins);
+        questData = qpUpdate(questData,actualStims(k),outcome);
+    end
+    
+    % get the next suggested stim from Q+
+    stim = qpQuery(questData);
+    
+    % write the suggeste stim to a new text file
+    nextStimNum = dir(fullfile(subjectPath,'stimLog'));
+    nextStimFileName = horzcat('nextStimuli',str(nextStimNum),'.txt');
+    fidw = fopen(nextStimFileName,'w');
+    fprintf(fidw,'%d',stim);
+    fclose(fidw);
+
+    
+    j = j + 1;
+
+    pause(0.01);
+end
