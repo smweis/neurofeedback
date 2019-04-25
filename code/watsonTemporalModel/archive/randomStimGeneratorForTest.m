dee9bfdb87280a835d815b71cc216b25812b3ac8
gkaguirre <gkaguirre@me.com>
2019-04-25 16:35:24 -0400

    Removing deprecated files
    Signed-off-by: gkaguirre <gkaguirre@me.com>

--- code/watsonTemporalModel/archive/randomStimGeneratorForTest.m
+++
@@ -1,9 +0,0 @@
-subjectPath = '/Users/nfuser/Documents/rtQuest/TOME_3040_TEST';
-for i = 1:23
-    stims = [2,4,8,16,32,64];
-    nextStim = stims(randi(6));
-    fid = fopen(fullfile(subjectPath,'actualStimuli.txt'),'a+');
-    fprintf(fid,'%d\n',nextStim);
-    fclose(fid);
-    pause(12);
-end
